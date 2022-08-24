0fd22677be16c40a6adacf05835fb3190b389537
vishnugithuber <111357262+vishnugithuber@users.noreply.github.com>
2022-08-24 18:36:58 +0530

    Add files via upload

---
+++ Application/Application/Program.cs
@@ -0,0 +1,21 @@
+﻿using Logicofapplication;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Application
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Sampleclass.Greet();
+            Sampleclass.Makefile();
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Application/Logicofapplication/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Logicofapplication
+{
+    public class Sampleclass
+    {
+        public static void Greet()
+        {
+            Console.WriteLine("helloyaar");
+
+        }
+        public static void Makefile()
+        {
+        
+            string path = @" E:\NET\Application\firstfile.txt";
+            File.Create(path);
+
+
+
+        }
+
+
+    }
+}
