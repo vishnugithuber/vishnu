20cc7ff64aaa4d1aa0cad9e29ac105e466babca3
vishnugithuber <111357262+vishnugithuber@users.noreply.github.com>
2022-09-06 10:31:41 +0530

    Add files via upload

---
+++ HandsonSep5/ClassLibrary1/Class1.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace ClassLibrary1
+{
+    public class Book
+    {
+        private int id;
+        public int BookId
+        {
+            get;
+            set;
+        }
+        private string bname;
+        public string Bname
+        {
+            get { return bname; }
+            set { bname = value; }
+        }
+        private float price;
+        public float Price
+        {
+            get { return price; }
+            set { price = value; }
+        }
+        private string author;
+        public string Author
+        {
+            get { return author; }
+            set { author = value; }
+        }
+        public void InsertBook(int Id, string Bookname, float Price)
+        {
+            id = Id;
+            bname = Bookname;
+            price = Price;
+
+        }
+        public void InsertBook(int Id, string Bookname, float Price, string Author)
+        {
+            id = Id;
+            bname = Bookname;
+            price = Price;
+            author = Author;
+
+        }
+        public void InsertBook(int Id, string Bookname)
+        {
+            id = Id;
+            bname = Bookname;
+        }
+        public Book(int id, string bname, float price, string author)
+        {
+            this.id = id;
+            this.bname = bname;
+            this.price = price;
+            this.author = author;
+        }
+        public void Display()
+        {
+            Console.WriteLine(id);
+            Console.WriteLine(bname);
+            Console.WriteLine(price);
+            Console.WriteLine(author);
+        }
+        public Book()
+        {
+
+        }
+    }
+    public class Novels : Book
+    {
+        private int noofpages;
+        public int Noofpages
+        {
+            get { return noofpages; }
+            set { noofpages = value; }
+        }
+        public void InsertBook(int id1, string bname1, float price1, int pages)
+        {
+            base.BookId = id1;
+            base.Bname = bname1;
+            base.Price = price1;
+            noofpages = pages;
+        }
+        public void Displays()
+        {
+            Console.WriteLine(BookId);
+            Console.WriteLine(Bname);
+            Console.WriteLine(Price);
+            Console.WriteLine(Noofpages);
+        }
+    }
+    public class Cart : Book
+    {
+        private int Customerid;
+        public int CustomerId
+        {
+            get { return Customerid; }
+            set { Customerid = value; }
+        }
+        private int bookingid;
+        public int BookingId
+        {
+            get { return bookingid; }
+            set { bookingid = value; }
+        }
+
+
+        List<Book> b = new List<Book>();
+        static int Count = 0;
+        public void Addtocart(int id, string bname, float price)
+        {
+            Book b1 = new Book();
+            b1.BookId = id;
+            b1.Bname = bname;
+            b1.Price = price;
+            b.Add(b1);
+            Count++;
+            Console.WriteLine($"Product added {Count}");
+
+        }
+
+
+        public void dis()
+        {
+            Console.WriteLine(CustomerId);
+            Console.WriteLine(BookingId);
+            foreach (var item in b)
+            {
+                Console.WriteLine(item.BookId);
+                Console.WriteLine(item.Bname);
+                Console.WriteLine(item.Price);
+            }
+        }
+        public void bookdis()
+        {
+            foreach (var item in b)
+            {
+                Console.Write($"{item.BookId}\t{item.Bname}\n");
+
+            }
+
+        }
+    }
+    public class Bookcategory
+    {
+        private int catid;
+        public int CatId
+        {
+            get;
+            set;
+        }
+        private string catname;
+        public string CatName
+        {
+            get { return catname; }
+            set { catname = value; }
+        }
+        private string desc;
+        public string Description
+        {
+            get;
+            set;
+        }
+        public void Display()
+        {
+            Console.WriteLine(catid);
+            Console.WriteLine(catname);
+            Console.WriteLine(desc);
+        }
+        public Bookcategory(int id, string name, string descr)
+        {
+            catid = id;
+            catname = name;
+            desc = descr;
+        }
+        public Bookcategory()
+        {
+
+        }
+
+
+
+
+
+    }
+}
---
+++ HandsonSep5/HandsonSep5/Program.cs
@@ -0,0 +1,51 @@
+﻿using ClassLibrary1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace HandsonSep5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book = new Book(1, "Sita", 200, "Amith");
+            book.Display();
+            Book b2 = new Book();
+            b2.InsertBook(2, "Alchemist", 150);
+            b2.Display();
+            Book b3 = new Book();
+            b3.InsertBook(3, "Rama Add", 100, "This product");
+            b3.Display();
+            Book b4 = new Book();
+            b4.InsertBook(4, "Room 105");
+            b4.Display();
+            Bookcategory b1 = new Bookcategory(1, "Drama", "Gener");
+            b1.Display();
+            Novels n1 = new Novels();
+            n1.InsertBook(1, "Rich dad and poor dad", 500);
+            n1.Display();
+            Novels n2 = new Novels();
+            n2.InsertBook(2, "Ikigai", 200, "Japanese");
+            n2.Display();
+            Cart c1 = new Cart();
+            c1.CustomerId = 44;
+            c1.BookingId = 10;
+            c1.Addtocart(1, "Rich dad and poor dad", 500);
+            c1.Addtocart(2, "psycology of money", 600);
+            c1.dis();
+            c1.bookdis();
+            Cart c2 = new Cart();
+            c2.CustomerId = 45;
+            c2.BookingId = 50;
+            c2.Addtocart(1, "Nari", 200);
+            c2.Addtocart(2, "Every one has a story", 180);
+            c2.dis();
+            c2.bookdis();
+            Console.ReadLine();
+        }
+    }
+}
