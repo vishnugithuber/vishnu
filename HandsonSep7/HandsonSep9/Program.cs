bebb5e4b3abaa1aa24a388d8c36814491690ea4c
vishnugithuber <111357262+vishnugithuber@users.noreply.github.com>
2022-09-07 21:05:19 +0530

    Add files via upload

---
+++ HandsonSep7/HandsonSep9/Program.cs
@@ -0,0 +1,65 @@
+﻿
+using System;
+using Library;
+using static Library.Class1;
+
+namespace HandsonSep9
+{
+    internal class Program
+    {
+        public delegate string ps5(string spo, string spo1);
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-------");
+            Console.WriteLine("Enter the string");
+            string a = Console.ReadLine();
+            str s1 = new str();
+            ps1 s2 = new ps1(s1.stringlen);
+            s2(a);
+            Console.WriteLine("-----");
+            Console.WriteLine("Enter username");
+            string b = Console.ReadLine();
+            ps1 s3 = new ps1(s1.greet);
+            s3(b);
+            Console.WriteLine("------");
+            Console.WriteLine("Enter the two numbers");
+            int c = Convert.ToInt32(Console.ReadLine());
+            int d = Convert.ToInt32(Console.ReadLine());
+            integer e = new integer();
+            ps2[] d3 = new ps2[3] { e.Add, e.Sub, e.Multiply };
+            ps2 d4 = (ps2)MulticastDelegate.Combine(d3);
+            d4(c, d);
+            Console.WriteLine("-------");
+            Console.WriteLine("Enter productId and product name");
+            Products mo = new Products();
+            mo.PId = Convert.ToInt32(Console.ReadLine());
+            mo.Name = Console.ReadLine();
+            ps3 poi = new ps3(mo.Display);
+            poi(mo);
+            Console.WriteLine("------");
+            Console.WriteLine("Enter the year,Month,date");
+            int yr = Convert.ToInt32(Console.ReadLine());
+            int mon = Convert.ToInt32(Console.ReadLine());
+            int dt = Convert.ToInt32(Console.ReadLine());
+            Employee emp = new Employee();
+            DateTime t = new DateTime(yr, mon, dt);
+            emp.Joining = t;
+            ps4 se = new ps4(emp.Expyrs);
+            int poiu = se(emp.Joining);
+            Console.WriteLine($"Years of exp {poiu}");
+            Console.WriteLine("------");
+            Console.WriteLine("Enter strings to Concatenate");
+            string yum = Console.ReadLine();
+            string muy = Console.ReadLine();
+            ps5 dpo = delegate (string so, string su)
+            {
+                return so + su;
+            };
+            string sod = dpo(yum, muy);
+            Console.WriteLine($"Concatenated string is {sod}");
+            
+            Console.ReadLine();
+        }
+    }
+}
+
---
+++ HandsonSep7/Library/Class1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Library
+{
+    public class Class1
+    {
+        public delegate void ps1(string s);
+        public delegate void ps2(int i, int j);
+        public delegate void ps3(Products p);
+        public delegate int ps4(DateTime t);
+        public class str
+        {
+            public void stringlen(string s)
+            {
+                Console.WriteLine(s.Length);
+                string s1 = s.ToUpper();
+                Console.WriteLine(s1);
+
+
+            }
+            public void greet(string s)
+            {
+                Console.WriteLine($"Hello {s}");
+            }
+        }
+        public class integer
+        {
+            public void Add(int i, int j)
+            {
+                Console.WriteLine($"Addition 0f {i},{j} is  {i + j}");
+            }
+            public void Sub(int i, int j)
+            {
+                if (i > j)
+                {
+                    Console.WriteLine($"Subtraction of {i},{j} is {i - j}");
+                }
+                else
+                {
+                    Console.WriteLine($"Subtraction of {i},{j} is  {j - i}");
+                }
+            }
+            public void Multiply(int i, int j)
+            {
+                Console.WriteLine($"Multiplication of {i},{j} is  {j * i}");
+            }
+
+        }
+        public class Products
+        {
+            public int PId
+            {
+                get;
+                set;
+            }
+            public string Name
+            {
+                get;
+                set;
+            }
+            public void Display(Products p)
+            {
+                Console.WriteLine($"Product id is {p.PId} and name of the product {p.Name}");
+            }
+        }
+        public class Employee
+        {
+            private DateTime _joining;
+            public DateTime Joining
+            {
+                get { return _joining; }
+                set { _joining = value; }
+
+            }
+            public int Expyrs(DateTime ji)
+            {
+                int p = DateTime.Now.Year - ji.Year;
+                return p;
+            }
+        }
+    }
+}
